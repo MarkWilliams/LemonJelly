692865be7e03dbc89ca8c7e28628a118eabb2ebc
Mark Williams <mark.williams@christie.nhs.uk>
2018-06-20 12:07:04 +0100

    Initial Commit

---
+++ nHapiFirstN35/nHapiFirstN35/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHapi.Base.Model;
+using NHapi.Base.Parser;
+using NHapi.Base;
+using NHapi.Model.V23;
+using NHapi.Model.V23.Message;
+using NHapi.Model.V23.Segment;
+
+namespace nHapiFirstN35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = @"MSH|^~\&|SENDING|SENDER|RECV|INST|20060228155525||QRY^R02^QRY_R02|1|P|2.3| QRD|20060228155525|R|I||||10^RD&Records&0126|38923^^^^^^^^&INST|||";
+            PipeParser parser = new PipeParser();
+            IMessage m = parser.Parse(message);
+            QRY_R02 qryR02 = m as QRY_R02;
+            Console.WriteLine(qryR02.QRD.QueryID.ToString());
+        }
+    }
+}
